d8eb1c027e08517756dc11117ff881e58f88828f
kuwaiti3sl <kuwaiticomunication@gmail.com>
2025-08-19 19:41:01 +0300

    done
    we used let sand const

---
+++ script.js
@@ -0,0 +1,15 @@
+let name = prompt("enter your name :");
+
+let age = prompt("enter your age :");
+
+const city = prompt (" your city ")
+
+console.log()
+
+alert (
+"hello, " + name + 
+" your age is " + age );
+
+
+
+
